858b617d9957b747fd4bdedc60aab9ccafe49896
Godwin <godwinaquinas@gmail.com>
2025-04-17 21:21:10 +0100

    add get route for contractTxId

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,9 @@ router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
+router.get('/contracts/:txId', async (req, res) => {
+  const txId = req.params.txId
+  res.send({ message: "hello world, I see you want state of txId " + txId})
+})
+
 module.exports = router;
